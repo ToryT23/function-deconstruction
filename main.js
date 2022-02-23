f7740db4b75516bac3db66cabe03c7f3fcd925a4
ToryT23 <torjtheu@gmail.com>
2022-02-23 00:17:55 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// Pick something you like to do, or something that you can do well that involves many steps. 
+// Your job is to deconstruct your hobby, job, or craft into the tasks required.
+
+// Write a function for each task.
+// In the function body, just console.log() a description of the task.
+// Invoke the tasks in the correct order.
+
+// how to cut hair
+// 1.) grab hair clipper
+// 2.) spray an all-in one cleaner lubercate on blades 
+// 3.) wipe off excess spray
+// 4.) lower lever to length of your desire
+// 5.) turn clipper on
+// 6.) take time cut hair 
+
+const grabClippers = () => {
+
+    console.log( "Grab a functional hair clipper")
+}
+
+const sprayForClipper = () => {
+    console.log("clean well")
+}
+
+const wipeOfClipper = () => {
+    console.log("make sure no excess spray or lube is on the clipper")
+
+}
+
+const useLeverToClipper = () => {
+    console.log("leave lever open or close")
+}
+
+const turnOnClipper = () => {
+    console.log("turn the clipper on")
+}
+
+const timeToCut = () => {
+    console.log("take your time on the masterpiece")
+}
+
+grabClippers()
+sprayForClipper()
+wipeOfClipper()
+useLeverToClipper()
+turnOnClipper()
+timeToCut()
